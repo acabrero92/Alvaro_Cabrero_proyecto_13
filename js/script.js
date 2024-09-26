289d755d43f504a80a4b0b5f04334d5e3566dc08
acabrero92 <alvarocabrero92@gmail.com>
2024-09-26 13:37:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previene el envío del formulario por defecto
+    alert('Formulario enviado!'); // Aquí puedes implementar la lógica para el envío del formulario
+});
